2bda4e9efe3b46d15dcdcd74ea37e23040cdfe46
sloning <i.96petrov@gmail.com>
2022-10-17 17:40:21 +0300

    lab2: add id validation

--- service2/src/main/java/com/example/service2/dto/LengthFilterDto.java
+++ service2/src/main/java/com/example/service2/dto/LengthFilterDto.java
@@ -4,11 +4,15 @@ import com.example.service2.model.Length;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+
 @Data
 @NoArgsConstructor
 public class LengthFilterDto {
 
+    @Min(1)
     private Integer idFrom;
+    @Min(1)
     private Integer idTo;
     private Length length;
 }
